5e1be72679232356569771214f5bfa9e87980a8a
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-24 16:04:19 -0400

    never verifies

--- fall2013/security/labs/lab3/part2/src/VerifyRSA.java
+++ fall2013/security/labs/lab3/part2/src/VerifyRSA.java
@@ -1,4 +1,8 @@
 package src;
+import java.util.*;
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
 
 // look at key factory
 
@@ -11,6 +15,35 @@ public class VerifyRSA {
      * Main
      */
     public static void main(String[] args) {
-	Uty.pln("hi");
+	if (args.length != 3) {
+	    Uty.plnPanic("USAGE: verify-rsa <key file> <signature file> <text file>");
+	}
+	
+	try {
+	    FileInputStream keyStream = new FileInputStream(args[0]);
+	    byte[] keyBytes = new byte[keyStream.available()];
+	    keyStream.read(keyBytes);
+	    keyStream.close();
+	    FileInputStream sigStream = new FileInputStream(args[1]);
+	    byte[] sigBytes = new byte[sigStream.available()];
+	    sigStream.read(sigBytes);
+	    sigStream.close();
+	    FileInputStream textStream = new FileInputStream(args[1]);
+	    byte[] textBytes = new byte[textStream.available()];
+	    textStream.read(textBytes);
+	    textStream.close();
+	    
+	    X509EncodedKeySpec pubKeyEncoded = new X509EncodedKeySpec(keyBytes);
+	    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	    PublicKey pubKey = keyFactory.generatePublic(pubKeyEncoded);
+	    Signature sig = Signature.getInstance("SHA1withRSA");
+	    sig.initVerify(pubKey);
+	    sig.update(textBytes);
+	    Uty.pln((sig.verify(sigBytes)) ? "OK" : "FAIL");
+
+	} catch (Exception e) {
+	    Uty.plnPanic("Error during init\n\t" + e);
+	}
+	
     }
 }
