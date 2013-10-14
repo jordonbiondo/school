5b98e741d7454c4b8a8d40a23fc1b46e76377501
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-14 10:01:44 -0400

    eos update

---
+++ fall2013/cis452/labs/lab5/sample1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define FOO 4096
+
+int main ()
+{
+   int shmId;
+   char *shmPtr;
+
+   if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) {
+      perror ("i can't get no..\n");
+      exit (1);
+   }
+   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) {
+      perror ("can't attach\n");
+      exit (1);
+   }
+
+   printf ("%d: value a: %lu\t value b: %lu\n", shmId,
+	   (unsigned long) shmPtr, (unsigned long) shmPtr + FOO);
+   pause();
+   /*
+    * Changes
+    */
+   struct shmid_ds super_struct;
+   if (shmctl(shmId, IPC_STAT, &super_struct) < 0) {
+     printf("eff\n");
+   } else {
+     printf("Size of segment: %d\n", super_struct.shm_segsz);
+   }
+   
+   
+   if (shmdt (shmPtr) < 0) {
+
+      perror ("just can't let go\n");
+      exit (1);
+   }
+   if (shmctl (shmId, IPC_RMID, 0) < 0) {
+      perror ("can't deallocate\n");
+      exit(1);
+   }
+
+   return 0;
+} 
